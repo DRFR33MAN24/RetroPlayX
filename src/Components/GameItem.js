785e5251611e45a7eb6dcfac27e663d0ce8f0125
Gordon Freeman <drfr33man@protonmail.com>
2022-10-31 00:23:16 +0200

    working on game detail screen

--- src/Components/GameItem.js
+++ src/Components/GameItem.js
@@ -1,42 +1,45 @@
 import React from 'react';
-import { Card, Text } from '@ui-kitten/components';
-import { View, Image, TouchableOpacity } from 'react-native';
-import { apple, smb3 } from '../Constants/images';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
-import { useNavigation } from '@react-navigation/native'
+import {Card, Text} from '@ui-kitten/components';
+import {View, Image, TouchableOpacity} from 'react-native';
+import {apple, smb3} from '../Constants/images';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faDownload} from '@fortawesome/free-solid-svg-icons';
+import {useNavigation} from '@react-navigation/native';
 
-function GameItem({ data }) {
-    const { id, title, image, downloads, system } = data;
-    const navigaion = useNavigation();
+function GameItem({data}) {
+  const {id, title, image, downloads, system} = data;
+  const navigaion = useNavigation();
 
-    return (
-        <View style={{ width: '100%', }} >
-            <Card onPress={() => navigaion.navigate('GameDetails', { ...data })}>
-
-                <View style={{ alignItems: 'center', }}>
-
-                    <Image source={smb3} resizeMode="cover" style={{ width: '100%', height: 64, borderRadius: 10 }} />
-                </View>
-                <Text category="s1">{title}</Text>
-                <View style={{ flexDirection: 'row', justifyContent: "space-between", marginTop: 10 }}>
-                    <View style={{ flexDirection: 'row', }}>
-
-                        <FontAwesomeIcon
-
-                            icon={faDownload}
-                            size={15}
-                            style={{ marginHorizontal: 5 }}
-
-                        />
-                        <Text category="label">{downloads}</Text>
-                    </View>
-                    <Text category="label">{system}</Text>
-                </View>
-
-            </Card>
+  return (
+    <TouchableOpacity
+      style={{width: '100%'}}
+      onPress={() => navigaion.navigate('GameDetails', {...data})}>
+      <View style={{alignItems: 'center'}}>
+        <Image
+          source={smb3}
+          resizeMode="cover"
+          style={{width: '100%', height: 64, borderRadius: 10}}
+        />
+      </View>
+      <Text category="s1">{title}</Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginTop: 10,
+        }}>
+        <View style={{flexDirection: 'row'}}>
+          <FontAwesomeIcon
+            icon={faDownload}
+            size={15}
+            style={{marginHorizontal: 5}}
+          />
+          <Text category="label">{downloads}</Text>
         </View>
-    );
+        <Text category="label">{system}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 export default GameItem;
--- src/Screens/GameDetails.js
+++ src/Screens/GameDetails.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, ScrollView, View, Image, Dimensions } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, ScrollView, View, Image, Dimensions} from 'react-native';
 import {
   Divider,
   Card,
@@ -8,13 +8,13 @@ import {
   useTheme,
   Button,
 } from '@ui-kitten/components';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
 
-import { BackIcon } from '../Components/NavigationComponents';
-import { appStyles } from '../Constants/style';
+import {BackIcon} from '../Components/NavigationComponents';
+import {appStyles} from '../Constants/style';
 
-export const GameDetails = ({ navigation, route }) => {
+export const GameDetails = ({navigation, route}) => {
   // const navigateDetails = () => {
   //   navigation.navigate('Details');
   // };
@@ -30,7 +30,7 @@ export const GameDetails = ({ navigation, route }) => {
 
     category,
     system,
-    size
+    size,
   } = route.params;
 
   // useEffect(() => {
@@ -38,42 +38,79 @@ export const GameDetails = ({ navigation, route }) => {
   //   setSurveys(surveysData);
   //   //console.log(surveyData);
   // }, []);
+  const VerticalDivider = () => (
+    <View
+      style={{
+        borderWidth: 1,
+        height: '100%',
+        borderColor: theme['text-basic-color'],
+      }}></View>
+  );
+
   return (
     <SafeAreaView
-      style={{ flex: 1, backgroundColor: theme['background-basic-color-4'] }}>
+      style={{flex: 1, backgroundColor: theme['background-basic-color-4']}}>
       <TopNavigation
         title={title}
         accessoryLeft={<BackIcon navigation={navigation} />}
       />
       <Divider />
 
-      <ScrollView style={{ flex: 1 }}>
-        <Text category="h2">{title}</Text>
-        <View style={{ flexDirection: 'row', justifyContent: "space-evenly", marginVertical: appStyles.s12 }}>
+      <ScrollView style={{flex: 1, padding: appStyles.p10}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            marginVertical: appStyles.s12,
+          }}>
           <Image
-            source={{ uri: image }}
+            source={{uri: image}}
             resizeMode="stretch"
-            style={{ width: 64, height: 64, borderRadius: appStyles.s12 }} />
-
-          <Button size="tiny">Install</Button>
+            style={{width: 64, height: 64, borderRadius: appStyles.s12}}
+          />
+          <Text category="h2">{title}</Text>
         </View>
-        <View style={{ flexDirection: 'row' }}>
-          {[...Array(rating)].map((e, i) => (
-            <FontAwesomeIcon
-              key={i}
-              icon={faStar}
-              size={20}
-
-            />
-          ))}
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            marginVertical: appStyles.s12,
+          }}>
+          <View>
+            <Text appearance="hint">Rating</Text>
+            <View style={{flexDirection: 'row'}}>
+              {[...Array(rating)].map((e, i) => (
+                <FontAwesomeIcon
+                  key={i}
+                  icon={faStar}
+                  size={15}
+                  style={{color: theme['color-primary-300']}}
+                />
+              ))}
+            </View>
+          </View>
+          <VerticalDivider />
+          <View>
+            <Text appearance="hint">Downloads</Text>
+            <Text appearance="hint">{downloads}</Text>
+          </View>
+          <VerticalDivider />
+          <View>
+            <Text appearance="hint">System</Text>
+            <Text appearance="hint">{system}</Text>
+          </View>
         </View>
+        <Button size="small">Install</Button>
         <Image
-          source={{ uri: image }}
+          source={{uri: image}}
           resizeMode="stretch"
-          style={{ width: '100%', height: 200 }}
+          style={{width: '100%', height: 200, marginVertical: appStyles.s12}}
         />
-        <View style={{ paddingHorizontal: 10 }}>
-          <Text category="p1" appearance="hint">{details}</Text>
+        <View style={{paddingHorizontal: 10}}>
+          <Text>About Game:</Text>
+          <Text category="p1" appearance="hint">
+            {details}
+          </Text>
           <Text>{category}</Text>
         </View>
       </ScrollView>
